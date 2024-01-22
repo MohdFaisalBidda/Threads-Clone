639de7aede12947f67509c88d37703257cab9d4c
MohdFaisalBidda <biddafaisal@gmail.com>
2024-01-22 22:58:14 +0530

    nav and navigation configured

--- threads-app/src/app/app.routes.ts
+++ threads-app/src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: "",
         loadComponent: () => import("./home/home.component").then(m => m.HomeComponent)
+    },
+    {
+        path: "about",
+        loadComponent: () => import("./about/about.component").then(m => m.AboutComponent)
     }
 ];
