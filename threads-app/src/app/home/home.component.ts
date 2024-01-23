8f4b7843b7a7ff110af380915d263c45cac4fba6
MohdFaisalBidda <biddafaisal@gmail.com>
2024-01-23 10:11:43 +0530

    nested comments added on reply

--- threads-app/src/app/home/home.component.ts
+++ threads-app/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommentComponent } from '../comment/comment.component';
+import { CommentComponent } from '../components/comment/comment.component';
 
 @Component({
   selector: 'app-home',
