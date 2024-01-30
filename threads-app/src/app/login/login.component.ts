8e3d5931a5580ed13e30cb95a2635a1c9d32b417
MohdFaisalBidda <biddafaisal@gmail.com>
2024-01-30 20:50:29 +0530

    loader added

--- threads-app/src/app/login/login.component.ts
+++ threads-app/src/app/login/login.component.ts
@@ -3,21 +3,25 @@ import { UsersService } from '../services/users.service';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from '../components/loader/loader.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule,LoaderComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   username: string = "";
   password: string = "";
+  loading: boolean = false;
 
   constructor(private userService: UsersService, private router: Router, private toastr: ToastrService) { }
 
   submitForm() {
+    this.loading = true;
     this.userService.findUserByName(this.username, this.password).subscribe((user) => {
       this.userService.saveUsertoLocalStorage(user)
       console.log(user);
@@ -28,6 +32,8 @@ export class LoginComponent {
         console.log("User not found", error);
         this.toastr.error(error.error.message, "Error")
       }
-    )
+    ).add(() => {
+      this.loading = false;
+    })
   }
 }
--- threads-app/src/app/register/register.component.ts
+++ threads-app/src/app/register/register.component.ts
@@ -3,21 +3,25 @@ import { FormsModule } from '@angular/forms';
 import { UsersService } from '../services/users.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { LoaderComponent } from '../components/loader/loader.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,LoaderComponent,CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   username: string = "";
   password: string = "";
+  loading: boolean = false;
 
   constructor(private userService: UsersService, private router: Router, private toastr: ToastrService) { }
 
   submitForm() {
+    this.loading = true;
     this.userService.createUser(this.username, this.password).subscribe((user) => {
       this.userService.saveUsertoLocalStorage(user);
       console.log(user);
@@ -28,6 +32,8 @@ export class RegisterComponent {
         console.log("User not found", error);
         this.toastr.error(error.error.message, "Error")
       }
-    )
+    ).add(() => {
+      this.loading = false;
+    })
   }
 }
