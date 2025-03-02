34ce7f4231d7687437a7f18dca7db8b33166019b
Mohd Faisal <biddafaisal@gmail.com>
2025-03-02 17:20:25 +0530

    Update environment.ts

--- threads-app/src/environments/environment.ts
+++ threads-app/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: "https://threads-server-0lqs.onrender.com"
+    // apiBaseUrl: "https://threads-server-0lqs.onrender.com"
+    apiBaseUrl: "https://bn-threadsapp.faisalbidda.xyz"
 }
